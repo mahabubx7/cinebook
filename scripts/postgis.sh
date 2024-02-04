321e69d6a09339e449f6d8dcf1eb9b1555bce013
Mahabub <mahabubx7@gmail.com>
2024-02-04 22:25:36 +0600

    fix(scripts): fixed scripts for proper grants

--- scripts/postgis.sh
+++ scripts/postgis.sh
@@ -6,3 +6,5 @@
 echo "Installing PostGIS extension in test database..."
 docker exec postgresql-db psql -U postgres -d cinebook_test -c "CREATE EXTENSION postgis;"
 echo "PostGIS installed!"
+docker exec postgresql-db psql -U postgres -d cinebook_test -c "GRANT TRUNCATE ON TABLE spatial_ref_sys TO developer;"
+echo "PostGIS permissions granted!"
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,3 +4,5 @@
 echo "Installing PostGIS extension..."
 docker exec postgresql-db psql -U postgres -d cinebook -c "CREATE EXTENSION postgis;"
 echo "PostGIS installed!"
+docker exec postgresql-db psql -U postgres -d cinebook -c "GRANT TRUNCATE ON TABLE spatial_ref_sys TO developer;"
+echo "PostGIS permissions granted!"
